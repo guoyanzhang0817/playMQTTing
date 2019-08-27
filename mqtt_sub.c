90c24260ae995697787015c6a3a9f2967128cc05
guoyanzhang <1746902011@qq.com>
2019-08-27 22:39:52 +0800

    one_callback_four_client

--- mqtt_sub.c
+++ mqtt_sub.c
@@ -2,51 +2,80 @@
 #include <stdlib.h>
 #include <string.h>
 #include "MQTTClient.h"
-#define ADDRESS     "tcp://localhost:1883"
-#define CLIENTID1    "ExampleClientSub1"
-#define CLIENTID2    "ExampleClientSub2"
-#define CLIENTID3    "ExampleClientSub3"
-#define CLIENTID4    "ExampleClientSub4"
-#define TOPIC1       "aaa"
-#define TOPIC2       "bbb"
-#define TOPIC3       "ccc"
-#define TOPIC4       "ddd"
-#define PAYLOAD     "Hello World!"
-#define QOS         1
-#define TIMEOUT     10000L
+#define ADDRESS          "tcp://localhost:1883"
+#define CLIENTID         "test_sub_and_pub"
+#define TOPIC            "test/subscribe/#"
+#define TOPIC1           "test/subscribe/aaa"
+#define TOPIC2           "test/subscribe/bbb"
+#define TOPIC3           "test/subscribe/ccc"
+#define TOPIC4           "test/subscribe/ddd"
+#define TOPIC5           "test/publish/aaa"
+#define TOPIC6           "test/publish/bbb"
+#define TOPIC7           "test/publish/ccc"
+#define TOPIC8           "test/publish/ddd"
+#define PAYLOAD          "Hello World!"
+#define QOS              1
+#define TIMEOUT          10000L
 volatile MQTTClient_deliveryToken deliveredtoken;
-
-
 
 void delivered(void *context, MQTTClient_deliveryToken dt)
 {
     printf("Message with token value %d delivery confirmed\n", dt);
     deliveredtoken = dt;
 }
-int msgarrvd(MQTTClient *p, char *topicName, int topicLen, MQTTClient_message *message)
+int msgarrvd(void *context, char *topicName, int topicLen, MQTTClient_message *message)
 {
+    MQTTClient *p = (MQTTClient)context;
     int i;
     char* payloadptr;
     MQTTClient_message pubmsg = MQTTClient_message_initializer;
     MQTTClient_deliveryToken token;
     printf("Message arrived\n");
-    printf("     topic: %s\n", topicName);
-    printf("   message: ");
+    printf("topic: %s\n", topicName);
+    printf("message: ");
     payloadptr = message->payload;
     for(i=0; i<message->payloadlen; i++)
     {
         putchar(*payloadptr++);
     }
     putchar('\n');
-    if (strcmp(topicName, "aaa") == 0)
+    if (strcmp(topicName, TOPIC1) == 0)
     {
-	printf("1\n");
-        pubmsg.payload = "huawei";
-        pubmsg.payloadlen = 7;
-        pubmsg.qos = 0;
+        pubmsg.payload = "huawei1";
+        pubmsg.payloadlen = 8;
+        pubmsg.qos = 2;
         pubmsg.retained = 0;
-        MQTTClient_publishMessage(*p, "hhh", &pubmsg, &token);
+        MQTTClient_publishMessage(*p, TOPIC5, &pubmsg, &token);
+	MQTTClient_waitForCompletion(*p, token, TIMEOUT);
     }
+    if (strcmp(topicName, TOPIC2) == 0)
+    {
+	pubmsg.payload = "huawei2";
+	pubmsg.payloadlen = 8;
+	pubmsg.qos = 2;
+	pubmsg.retained = 0;
+	MQTTClient_publishMessage(*p, TOPIC6, &pubmsg, &token);
+	MQTTClient_waitForCompletion(*p, token, TIMEOUT);
+    }
+    if (strcmp(topicName, TOPIC3) == 0)
+    {
+	pubmsg.payload = "huawei3";
+	pubmsg.payloadlen = 8;
+	pubmsg.qos = 2;
+	pubmsg.retained = 0;
+	MQTTClient_publishMessage(*p, TOPIC7, &pubmsg, &token);
+	MQTTClient_waitForCompletion(*p, token, TIMEOUT);
+    }
+    if (strcmp(topicName, TOPIC4) == 0)
+    {
+	pubmsg.payload = "huawei4";
+	pubmsg.payloadlen = 8;
+	pubmsg.qos = 2;
+	pubmsg.retained = 0;
+	MQTTClient_publishMessage(*p, TOPIC8, &pubmsg, &token);
+	MQTTClient_waitForCompletion(*p, token, TIMEOUT);
+    }
+
     MQTTClient_freeMessage(&message);
     MQTTClient_free(topicName);
     return 1;
@@ -59,54 +88,27 @@ void connlost(void *context, char *cause)
 
 int main(int argc, char* argv[])
 {
-    MQTTClient client1;
-    MQTTClient client2;
-    MQTTClient client3;
-    MQTTClient client4;
-    MQTTClient_connectOptions conn_opts1 = MQTTClient_connectOptions_initializer;
-    MQTTClient_connectOptions conn_opts2 = MQTTClient_connectOptions_initializer;
-    MQTTClient_connectOptions conn_opts3 = MQTTClient_connectOptions_initializer;
-    MQTTClient_connectOptions conn_opts4 = MQTTClient_connectOptions_initializer;
+    MQTTClient client;
+    MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
     int rc;
     int ch;
-    MQTTClient_create(&client1, ADDRESS, CLIENTID1, MQTTCLIENT_PERSISTENCE_NONE, NULL);
-    MQTTClient_create(&client2, ADDRESS, CLIENTID2, MQTTCLIENT_PERSISTENCE_NONE, NULL);
-    MQTTClient_create(&client3, ADDRESS, CLIENTID3, MQTTCLIENT_PERSISTENCE_NONE, NULL);
-    MQTTClient_create(&client4, ADDRESS, CLIENTID4, MQTTCLIENT_PERSISTENCE_NONE, NULL);
-    conn_opts1.keepAliveInterval = 20;
-    conn_opts1.cleansession = 1;
-    conn_opts2.keepAliveInterval = 20;
-    conn_opts2.cleansession = 1;
-    conn_opts3.keepAliveInterval = 20;
-    conn_opts3.cleansession = 1;
-    conn_opts4.keepAliveInterval = 20;
-    conn_opts4.cleansession = 1;
-    MQTTClient_setCallbacks(client1, &client1, connlost, msgarrvd, delivered);
-    MQTTClient_setCallbacks(client2, NULL, connlost, msgarrvd, delivered);
-    MQTTClient_setCallbacks(client3, NULL, connlost, msgarrvd, delivered);
-    MQTTClient_setCallbacks(client4, NULL, connlost, msgarrvd, delivered);
-
-    MQTTClient_connect(client1, &conn_opts1);
-    MQTTClient_connect(client2, &conn_opts2);
-    MQTTClient_connect(client3, &conn_opts3);
-    MQTTClient_connect(client4, &conn_opts4);
-
-    MQTTClient_subscribe(client1, TOPIC1, QOS);
-    MQTTClient_subscribe(client2, TOPIC2, QOS);
-    MQTTClient_subscribe(client3, TOPIC3, QOS);
-    MQTTClient_subscribe(client4, TOPIC4, QOS);
+    rc = MQTTClient_create(&client, ADDRESS, CLIENTID, MQTTCLIENT_PERSISTENCE_NONE, NULL);
+    conn_opts.keepAliveInterval = 20;
+    conn_opts.cleansession = 1;
+    rc += MQTTClient_setCallbacks(client, &client, connlost, msgarrvd, delivered);
+    rc += MQTTClient_connect(client, &conn_opts);
+    rc += MQTTClient_subscribe(client, TOPIC, QOS);
+    if (rc != MQTTCLIENT_SUCCESS)
+    {
+	printf("failed to subscribe, return error code %d.\n", rc);
+	return -1;
+    }
     do
     {
         ch = getchar();
     } while(ch!='Q' && ch != 'q');
-    MQTTClient_disconnect(client1, 10000);
-    MQTTClient_disconnect(client2, 10000);
-    MQTTClient_disconnect(client3, 10000);
-    MQTTClient_disconnect(client4, 10000);
-    MQTTClient_destroy(&client1);
-    MQTTClient_destroy(&client2);
-    MQTTClient_destroy(&client3);
-    MQTTClient_destroy(&client4);
+    MQTTClient_disconnect(client, 10000);
+    MQTTClient_destroy(&client);
     return rc;
 }
 
